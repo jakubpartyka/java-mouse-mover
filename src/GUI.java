d5c097587b75ded39a611c9d74fa5794fc8e6f39
jakubpartyka <j.partyka20@gmail.com>
2020-12-23 16:27:56 +0100

    GUI displayed

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,21 @@
 import javax.swing.*;
 
-public class GUI {
+public class GUI implements Runnable {
+    private JFrame frame;
     private JTextField activityIntervalField;
     private JButton STARTButton;
     private JButton EXITButton;
+    private JLabel statusLabel;
+    private JPanel panel;
+
+    @Override
+    public void run() {
+        frame = new JFrame("Java Mouse Mover");
+        frame.add(panel);
+        frame.setSize(400,400);
+        frame.setResizable(false);
+        frame.setAlwaysOnTop(true);
+        frame.setVisible(true);
+
+    }
 }
